bc466fd178a1d0d8c5502f9d964b4133f24b2d28
KarimOdisseo00 <150606031+KarimOdisseo00@users.noreply.github.com>
2023-12-17 17:14:40 +0100

    Particle.hpp
    Final Version

--- NBody AMSC/Particle.hpp
+++ NBody AMSC/Particle.hpp
@@ -1,5 +1,3 @@
-// Committed by Giuseppe Di Paola
-
 #include "Arrows.hpp"
 #include "Constants.hpp"
 #include <iostream>
@@ -23,14 +21,14 @@ class Particle {
         Particle(unsigned int id, const Arrows<dim>& pos, const Arrows<dim>& vel, const Arrows<dim>& acc, const Arrows<dim>& coeff, double m): ID(id), position(pos), velocity(vel), accelleration(acc),coefficients(coeff), mass(m) {}
 
         //Metodo per il calcolo dei coefficienti che richiede come argomento un'altra particella e torna una Arrow di coefficienti
-        void calcCoefficients(Particle& otherParticle){
+        Arrows<dim> calcCoefficients(const Particle& otherParticle){
             //Calcolo in primis una arrow che abbia come componenti le differenze tra le coordinate
             Arrows<dim> differences = Arrows<dim>();
             for(unsigned int i=0; i<dim; ++i){
                 differences[i]=position[i]-otherParticle.position[i];
             }
             double distance = differences.normEclidea();
-            coefficients = differences * otherParticle.mass /(distance*distance*distance);
+            return differences * otherParticle.mass /(distance*distance*distance);
         }
         
         
@@ -64,8 +62,8 @@ class Particle {
 
         //Metodo per calcolare future Velocità. Si basa sulle leggi di moto del m.u.a. La velocità V_t sarà data da V_t_0 + Accellerazione * dt
         Arrows<dim> calcNextVelocity(double& dt){
-            Arrows<dim> futureAcc = Arrows<dim>();
-            return futureAcc = velocity + accelleration * dt;
+            Arrows<dim> futureVel = Arrows<dim>();
+            return futureVel = velocity + accelleration * dt;
         } 
 
         //Metodo per calcolare future Posizioni. Si basa sulle leggi di moto del m.u.a. La posizione S_t sarà data da S_t_0 + V_t_0 * Accellerazione + 0.5 * Accellerazione * dt * dt
@@ -86,8 +84,45 @@ class Particle {
 
         //Metodo per set valori coefficienti
         void setToZero(){
-            coefficients = Arrows<dim>({0.0, 0.0, 0.0});
-            accelleration = Arrows<dim>({0.0, 0.0, 0.0});
+            if constexpr (dim == 1){
+                coefficients = Arrows<dim>({0.0});
+                accelleration = Arrows<dim>({0.0});
+            }
+            else if constexpr (dim==2)
+            {
+                coefficients = Arrows<dim>({0.0, 0.0});
+                accelleration = Arrows<dim>({0.0, 0.0});
+            }
+            else if constexpr(dim==3){
+                coefficients = Arrows<dim>({0.0, 0.0, 0.0});
+                accelleration = Arrows<dim>({0.0, 0.0, 0.0});
+            }
+
         }
+        
+        void coefficientsSetter (Arrows<dim>& otherCoefficients){
+            coefficients=otherCoefficients;
+        }
+        void positionsSetter (Arrows<dim>& otherPositions){
+            position=otherPositions;
+        }
+
+        Arrows<dim> getCoefficients() const {
+            return coefficients;
+        }
+
+        double getPositionCoordinate(const unsigned int i) const{
+            return position[i];
+        }
+
+        auto getID() const{
+            return ID;
+        }
+
+        auto getMass() const{
+            return mass;
+        }
+
+        
 };
 #endif
