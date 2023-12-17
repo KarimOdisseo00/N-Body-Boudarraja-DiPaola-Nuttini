5e085e7afb74f19a1ba9c6a0358e62137949a39f
KarimOdisseo00 <150606031+KarimOdisseo00@users.noreply.github.com>
2023-12-17 17:15:10 +0100

    Update simulationfunctions.hpp

--- NBody AMSC/simulationfunctions.hpp
+++ NBody AMSC/simulationfunctions.hpp
@@ -1,5 +1,3 @@
-//Committed by Elena Nuttini
-
 // File adibito alla gestione delle funzioni usate globalmente per fare la simulazione. 
 // possiamo già identificare un metodo utile per far proseguire la simulazione dove specifica ciò che accade per ogni timestep
 
@@ -10,6 +8,9 @@
 #include "Constants.hpp"
 #include <vector>
 #include <random>
+#include <iostream>
+#include <fstream>
+#include <string>
 
 
 class simulationfunctions{
@@ -20,16 +21,17 @@ public:
     static std::vector<Particle<dim>> generateParticles() {
         std::vector<Particle<dim>> particles;
         //PER DEBUGGING
-        //particles.push_back(Particle<dim>(1, Arrows<dim>({1.0, 2.0, 3.0}), Arrows<dim>({0.0, 0.0, 0.0}), Arrows<dim>({0.0,0.0,0.0}), Arrows<dim>({0.0,0.0,0.0}), 5.0));
-        //particles.push_back(Particle<dim>(2, Arrows<dim>({3.0, 2.0, 1.0}), Arrows<dim>({0.0, 0.0, 0.0}), Arrows<dim>({0.0,0.0,0.0}), Arrows<dim>({0.0,0.0,0.0}), 7.0));
-        
+        /*particles.push_back(Particle<dim>(1, Arrows<dim>({1.0, 2.0, 3.0}), Arrows<dim>({0.0, 0.0, 0.0}), Arrows<dim>({0.0,0.0,0.0}), Arrows<dim>({0.0,0.0,0.0}), 5.0));
+        particles.push_back(Particle<dim>(2, Arrows<dim>({3.0, 2.0, 1.0}), Arrows<dim>({0.0, 0.0, 0.0}), Arrows<dim>({0.0,0.0,0.0}), Arrows<dim>({0.0,0.0,0.0}), 7.0));
+        particles.push_back(Particle<dim>(3, Arrows<dim>({4.0, 5.0, 6.0}), Arrows<dim>({0.0, 0.0, 0.0}), Arrows<dim>({0.0,0.0,0.0}), Arrows<dim>({0.0,0.0,0.0}), 10.0));
+        particles.push_back(Particle<dim>(4, Arrows<dim>({6.0, 5.0, 4.0}), Arrows<dim>({0.0, 0.0, 0.0}), Arrows<dim>({0.0,0.0,0.0}), Arrows<dim>({0.0,0.0,0.0}), 1.0));*/
         // Metodo che genera un numero prefissato di particelle di dimensione DIM con valori casuali
         std::random_device rd;
         std::mt19937 gen(rd());
         std::uniform_real_distribution<double> distribution(-10.0, 10.0); // Cambia il range se necessario
         std::uniform_real_distribution<double> massDistribution(0.1, 10.0); // Massa compresa tra 0.1 e 10.0 (valori arbitrari)
 
-        for (int i = 0; i < 1000000; ++i) {
+        for (unsigned int i = 0; i < numberOfParticles; ++i) {
             // Genera valori casuali per posizione, velocità, massa, ecc.
             Arrows<dim> randomPosition;
             Arrows<dim> randomVelocity;
@@ -45,38 +47,150 @@ public:
             double randomMass = massDistribution(gen);
 
             // Crea e aggiungi una particella con valori casuali alla collezione
-            particles.push_back(Particle<dim>(i + 1, randomPosition, randomVelocity, Arrows<dim>({0.0, 0.0, 0.0}), Arrows<dim>({0.0, 0.0, 0.0}), randomMass));
+            particles.push_back(Particle<dim>(i + 1, randomPosition, randomVelocity, Arrows<dim>(), Arrows<dim>(), randomMass));
         }
 
 
         return particles;
     }
 
-    //Metodo che esegue la simulazione. Tale metodo provvederà a chiamare il generatore di Particles.
-    static void executeSim(){
+    //Metodo che esegue la simulazione. Tale metodo provvederà a chiamare il generatore di Particles. PER ORA SOLVER BASE!!!
+    static void doSim(){
         
         std::vector<Particle<dim>> particles = std::vector<Particle<dim>>();
+        std::vector<double> positions = std::vector<double>();
+
         particles = generateParticles();
         for(Particle particle : particles){
             particle.setToZero();
             particle.print();
             std::cout<<" "<<std::endl;
         }
-        std::cout<<"!!!! INIZIO DELLA SIMULAZIONE !!!!"<<std::endl;
-        std::cout<<" "<<std::endl;
-        for(int i = 0; i<particles.size(); ++i){
-            particles[i].calcCoefficients(particles[i+1]);
-            particles[i].calcAccelleration();
-            //ATTENZIONE DEVO GESTIRE L'ULTIMA!
-        }
-        double dt = 10;
-        for(Particle particle : particles){
-            particle.updateVelocity(dt);
-            particle.updatePosition(dt);
-            particle.print();
+
+        
+        
+        for(unsigned int i=0 ; i<cycles; i++){
+            std::cout<<"!!!! INIZIO DELLA SIMULAZIONE !!!!"<<std::endl;
             std::cout<<" "<<std::endl;
+            std::cout<<"CICLO "<< i << ":" << std::endl;
+            std::cout<<" "<<std::endl;
+            
+            stepSim(particles,positions);
+            updateSim(particles);
+        }
+        writePositionToTXT(positions);
+    
+    }
+    
+    //Salvataggio delle posizioni su una vector. utile per generare il file txt che dovrà essere letto per l'animazione grafica
+    static void memPos(const std::vector<Particle<dim>> particles, std::vector<double>& positions){
+        for(Particle particle : particles){
+            for(unsigned int i=0; i<dim; ++i){
+                positions.emplace_back(particle.getPositionCoordinate(i));
+            }
         }
     }
+
+    //Algoritmo Base
+    static void stepSim(std::vector<Particle<dim>>& particles,std::vector<double>& positions){  
+        memPos(particles,positions);//Riempi le posizioni a ciclo
+        for(unsigned int i = 0; i<particles.size(); ++i){
+            Arrows<dim> temp = Arrows<dim>();
+            for(unsigned int j=0; j<particles.size(); ++j){
+                if(i==j){continue;}
+                temp += particles[i].calcCoefficients(particles[j]);
+            }
+            particles[i].coefficientsSetter(temp);
+            particles[i].calcAccelleration();
+        }
+    } 
+
+    //Algoritmo ridotto
+    static void reducedSim(std::vector<Particle<dim>>& particles){
+        unsigned int middleIndex = particles.size() / 2;
+        for(unsigned int i = 0; i < middleIndex; ++i){
+            Arrows<dim> temp = Arrows<dim>();
+            Arrows<dim> temp2 = Arrows<dim>();
+            for(unsigned int j = i + 1; j < particles.size(); ++j){
+                temp += particles[i].calcCoefficients(particles[j]);
+                particles[i].coefficientsSetter(temp);
+                temp2 = particles[j].getCoefficients() - particles[i].calcCoefficients(particles[j]);
+                particles[j].coefficientsSetter(temp2);
+            }
+            particles[i].calcAccelleration();
+            particles[particles.size()-i-1].calcAccelleration();
+        }
+    }
+ 
+        
+    static void updateSim(std::vector<Particle<dim>>& particles){ 
+        double local_dt = dt;
+        for(Particle<dim>& particle : particles){
+            particle.updateVelocity(local_dt);
+            particle.updatePosition(local_dt);
+            particle.print();
+            std::cout<<" "<<std::endl;
+            particle.setToZero();
+        }
+    } 
+    
+   
+    void solveCollisions(std::vector<Particle<dim>> particles){
+        
+    }
+    
+    //Determinazione del numero di cicli. Deve essere fornito un lasso di tempo totale e un dt
+    unsigned int numberOfCycles(double totalTime, double dt){
+        double temp = totalTime/dt;
+        return static_cast<unsigned int>(std::floor(temp));
+    }
+    
+    //Scrivi le coordinate delle particelle su file TXT. Necessario per implementare la graficazione della simulazione
+    static void writePositionToTXT(const std::vector<double> positions) {
+        std::ofstream outputFile("positions.txt");
+        if (!outputFile.is_open()) {
+            std::cerr << "Impossibile aprire il file!" << std::endl;
+            return;
+        }
+
+        int coordinates = dim;
+        int rows = positions.size() / coordinates;
+        int dif = 0;
+        int cycle = 0;
+        int counter = cycle + 1;
+
+        for (int i = 0; i < rows; ++i) {
+            outputFile << cycle << ","; // Scrivi il numero del ciclo
+            if(i >= numberOfParticles)
+            {
+                outputFile << i - dif << ","; //Scrivi l'ID della particella dopo il primo ciclo
+            } 
+            else 
+            {
+                outputFile << i << ","; // Scrivi l'ID della particella per il primo ciclo
+            }
+
+            for (int j = 0; j < coordinates; ++j) {
+                outputFile << positions[i * coordinates + j]; // Scrivi le coordinate
+                if (j != coordinates - 1) {
+                    outputFile << ",";
+                }
+            }
+            outputFile << std::endl; // Vai a capo dopo ogni riga
+            
+
+            if(i == numberOfParticles*counter - 1)
+            {
+                dif = counter*numberOfParticles;
+                ++counter;
+                ++cycle;
+            }
+            
+        }
+
+        outputFile.close();
+    }
+
 };
 
 #endif // PARTICLE_UTIL_HPP
